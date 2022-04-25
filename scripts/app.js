f4c32e397ce3720b88eda47cb4828cacac1800f4
dbriski <dbriski26@gmail.com>
2022-04-25 17:18:11 +0200

    Add img, fix naming convention

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,32 @@
-const submitTask = document.getElementById('task-modal');
-const cancelBtn = document.getElementById('cancel-btn');
-const addBtn = document.getElementById('confirm-btn');
+// DOM constants
 const writeTaskBtn = document.getElementById('write-task');
 const backdrop = document.getElementById('backdrop');
-const taskModalEl = document.getElementById('modal-style');
-const backlogEl = document.getElementById('backlog-container');
-const inProgressEl = document.getElementById('inprogress-container');
-const completedEl = document.getElementById('completed-container');
+const taskModal = document.getElementById('task-modal');
+const cancelBtn = document.getElementById('cancel-btn');
+const addBtn = document.getElementById('confirm-btn');
+const taskModalEl = document.getElementById('modal-container');
+const backlogEl = document.getElementById('backlog');
+const inProgressEl = document.getElementById('inprogress');
+const completedEl = document.getElementById('completed');
 const containers = document.querySelectorAll('.task-container');
 
+// Global variables
 let idVal = 1;
 let draggedEl;
-
-let currentActive = 1;
-
 let tasks = [];
 
-function createTask(task) {
+// Backdrop and  closing TaskModal interface
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
+const exitTaskModal = () => {
+  toggleBackdrop();
+  taskModalEl.classList.remove('visible');
+};
+
+// Create task from TaskModal interface
+const createTask = (task) => {
   const taskTemplate = document.getElementById('template');
   const taskBody = document.importNode(taskTemplate.content, true);
   taskBody.querySelector('div').setAttribute('id', task.id);
@@ -24,56 +34,41 @@ function createTask(task) {
   taskBody.querySelector('p').textContent = task.value;
   backlogEl.append(taskBody);
   tasks.push(task);
-  submitTask.querySelector('input').value = '';
-  submitTask.querySelector('#title-field input').value = '';
-  submitTask.querySelector('#description-field textarea').value = '';
-}
+  taskModal.querySelector('input').value = '';
+  taskModal.querySelector('#title-field input').value = '';
+  taskModal.querySelector('#description-field textarea').value = '';
+};
 
-function renderTask() {
-  console.log(submitTask.querySelector('#title-field input').value);
+const renderTask = () => {
   if (idVal === 0) {
     idVal = 1;
   }
   const task = {
     id: idVal++,
-    title: submitTask.querySelector('#title-field input').value,
-    value: submitTask.querySelector('#description-field textarea').value,
+    title: taskModal.querySelector('#title-field input').value,
+    value: taskModal.querySelector('#description-field textarea').value,
   };
   createTask(task);
-}
-
-const exitTaskModal = () => {
-  toggleBackdrop();
-  taskModalEl.classList.remove('visible');
 };
 
-const toggleBackdrop = () => {
-  backdrop.classList.toggle('visible');
-};
-
-function taskRender() {
+// Task Handler functions
+function taskHandler() {
   containers.forEach((cont) => {
     const selected = cont.querySelectorAll('.task-box');
-    console.log(selected);
     for (const task of selected) {
-      console.log(task);
-      const removeBtn = task.querySelector('#title-box button:first-of-type');
-      const prevBtn = task.querySelector('#button-box button:first-of-type');
-      const nextBtn = task.querySelector('#button-box button:last-of-type');
+      const removeBtn = task.querySelector('#title-row button:first-of-type');
+      const prevBtn = task.querySelector('#button-row button:first-of-type');
+      const nextBtn = task.querySelector('#button-row button:last-of-type');
       const classes = task.classList;
       const appendNextFun = appendNext.bind(this, task);
       const appendPrevFun = appendPrev.bind(this, task);
-      // console.log(classes)
       if (classes[1] == 'active') {
-        console.log('bla');
         nextBtn.removeEventListener('click', appendNextFun);
         prevBtn.removeEventListener('click', appendPrevFun);
         continue;
       }
 
       task.classList.add('active');
-      console.log(classes);
-      console.log(task);
       nextBtn.addEventListener('click', appendNextFun);
       prevBtn.addEventListener('click', appendPrevFun);
       removeBtn.addEventListener('click', removeTask.bind(this, task));
@@ -84,45 +79,44 @@ function taskRender() {
 }
 
 function appendNext(task) {
-  if (task.parentElement.id == 'backlog-container') {
+  if (task.parentElement.id == 'backlog') {
     inProgressEl.append(task);
     task.classList.add('inprogress-active');
-  } else if (task.parentElement.id == 'inprogress-container') {
+  } else if (task.parentElement.id == 'inprogress') {
     completedEl.append(task);
     task.classList.remove('inprogress-active');
     task.classList.add('completed-active');
   }
-  taskRender();
+  taskHandler();
 }
 
 function appendPrev(task) {
-  console.log(task.parentElement.id);
-  if (task.parentElement.id == 'completed-container') {
-    console.log('bla');
+  if (task.parentElement.id == 'completed') {
     inProgressEl.append(task);
     task.classList.remove('completed-active');
     task.classList.add('inprogress-active');
-  } else if (task.parentElement.id == 'inprogress-container') {
+  } else if (task.parentElement.id == 'inprogress') {
     backlogEl.append(task);
     task.classList.remove('inprogress-active');
     task.classList.add('backlog-active');
   }
-  taskRender();
+  taskHandler();
 }
 
 function removeTask(task) {
   task.remove();
 }
 
-function connectDrag(task) {
+// Drag and Drop logic
+const connectDrag = (task) => {
   task.addEventListener('dragstart', (event) => {
     event.dataTransfer.setData('text/plain', task.id);
     event.dataTransfer.effectAllowed = 'move';
     draggedEl = event.target;
   });
-}
+};
 
-function connectDroppable() {
+const connectDroppable = () => {
   document.addEventListener('dragover', (event) => {
     if (event.dataTransfer.types[0] === 'text/plain') {
       event.preventDefault();
@@ -130,16 +124,16 @@ function connectDroppable() {
   });
 
   document.addEventListener('dragenter', (event) => {
-    if (event.target.className == 'task-container') {
+    console.log(event.target)
+    console.log(event.relatedTarget)
+    if (event.target.classList[0] == 'task-container') {
       event.target.classList.add('droppable');
       event.preventDefault();
     }
   });
 
   document.addEventListener('dragleave', (event) => {
-    if (event.target.classList[0] == 'task-container') {
-      event.target.classList.remove('droppable');
-    }
+    event.target.classList.remove('droppable');
   });
 
   document.addEventListener('drop', (event) => {
@@ -149,9 +143,11 @@ function connectDroppable() {
       event.target.appendChild(draggedEl);
     }
   });
-}
+};
 
-// Button events
+connectDroppable();
+
+// TaskModal events
 writeTaskBtn.addEventListener('click', () => {
   toggleBackdrop();
   taskModalEl.classList.add('visible');
@@ -165,11 +161,9 @@ backdrop.addEventListener('click', () => {
   exitTaskModal();
 });
 
-submitTask.addEventListener('submit', (event) => {
+taskModal.addEventListener('submit', (event) => {
   event.preventDefault();
   renderTask();
-  taskRender();
+  taskHandler();
   exitTaskModal();
 });
-
-connectDroppable();
